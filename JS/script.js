3d35396f15ce5bc286de5a25879b10d0c76f4169
Becky Friesen <beckyfriesen@Beckys-MacBook-Pro.local>
2021-02-03 13:46:51 -0700

    javascript iterations

--- JS/script.js
+++ JS/script.js
@@ -1,57 +1,24 @@
-console.log("connected");
+var jokes = [
+    "Why was the cat sitting on the computer?",
+    "To keep an eye on the mouse!",
+    "Why don't cats play poker in the jungle?",
+    "There are too many cheetahs!",
+    "Why was the cat so agitated?",
+    "He was in a bad mewd!"
+    ]
+console.log(jokes.length)
 
-var $joke1 = $("#joke1");
-var $answer1 = $("#answer1");
-var $joke2 = $("#joke2");
-var $answer2 = $("#answer2");
-var $joke3 = $("#joke3");
-var $answer3 = $("#answer3");
-var $next = $("#next");
-var $next2 = $("#next2");
-var $next3 = $("#next3");
+var $joke = $(".joke")
+var x = 0;
 
+$joke.text(jokes[0])
 
-$joke1.on("click", function(){
-    console.log("click")
-    $answer1.fadeIn();
-    $("body").css("background-color", "#B5E2FA");
-    $next.css("display", "inline-block");
-})
-
-$next.on("click", function(){
-    $joke1.hide();
-    $answer1.hide();
-    $joke2.show();
-    $next.hide();
-})
-
-$joke2.on("click", function(){
-    console.log("click")
-    $answer2.fadeIn();
-    $("body").css("background-color", "#c4eed9");
-    $("#next2").css("display", "inline-block");
-})
-
-
-$next2.on("click", function(){
-    $joke2.hide();
-    $answer2.hide();
-    $joke3.show();
-    $next2.hide();
+$joke.on("click", function(){
+    x++;
     
-})
-
-$joke3.on("click", function(){
-    console.log("click")
-    $answer3.fadeIn();
-    $("body").css("background-color", "#ffeeb3");
-    $next3.css("display", "inline-block");
-})
-
-$next3.on("click", function(){
-    $joke3.hide();
-    $answer3.hide();
-    $joke1.show();
-    $next3.hide();
-    $("body").css("background-color", "#ffc4d3");
+    if (x == jokes.length){
+        x = 0
+    }
+    console.log(x)
+    $(this).text(jokes[x])
 })
